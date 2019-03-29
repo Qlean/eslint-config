71c76c0ed03e507bf3dd822f952d5065d6337cd5
Vitaliy Pakshaev <pakshaev.vs@gmail.com>
2019-03-29 20:15:14 +0300

    removes unnecessary rules, extends configs from "prettier/react"

--- packages/eslint-config-qlean-react-native/index.js
+++ packages/eslint-config-qlean-react-native/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   "parser": "babel-eslint",
-  "extends": ["airbnb", "prettier"],
+  "extends": ["airbnb", "prettier", "prettier/react"],
   "plugins": [
     "react-native",
     "no-loops"
@@ -9,21 +9,10 @@ module.exports = {
     "global-require": 0,
     "react/require-default-props": 0,
     "react/jsx-filename-extension": [2, { "extensions": [".js"] }],
-    "react/jsx-curly-spacing": [2, "never", { "allowMultiline": false }],
-    "react/jsx-tag-spacing": [2, {
-      "closingSlash": "never",
-      "beforeSelfClosing": "never",
-      "afterOpening": "never"
-    }],
     "react-native/split-platform-components": 2,
     "react-native/no-inline-styles": 2,
     "react-native/no-color-literals": 2,
     "no-loops/no-loops": 2,
-    "react/jsx-curly-spacing": 0,
-    "react/jsx-tag-spacing": 0,
-    "react/jsx-closing-tag-location": 0,
-    "react/jsx-wrap-multilines": 0,
-    "react/jsx-one-expression-per-line": 0,
     "react/destructuring-assignment": ["always", { "ignoreClassFields": true }]
   },
   "globals": {
