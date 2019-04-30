3aa4abdb424fa6f44a4ccb9f757f428528fc2287
Alexander Tewpin <alex.tewpin@gmail.com>
2019-04-30 14:29:39 +0300

    public-to-private (#10)
    * Public to private
    
    * Commas

---
+++ packages/eslint-config-react-native/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  plugins: [
+    'react-native',
+    'no-loops',
+  ],
+  rules: {
+    'global-require': 0,
+    'react/jsx-filename-extension': [2, { 'extensions': ['.js'] }],
+    'react/destructuring-assignment': ['always', { 'ignoreClassFields': true }],
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2,
+    'no-loops/no-loops': 2,
+  },
+  globals: {
+    '__DEV__': true,
+  },
+};
