b0d6f136afa677d3137586a30ae717c4fbbb55c5
Vitaliy Pakshaev <pakshaev.vs@gmail.com>
2019-08-02 11:30:34 +0300

    react hooks plugin

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,22 +1,28 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   extends: [
-    'airbnb-base',
-    'airbnb-base/rules/strict',
-    'airbnb/rules/react',
-    'prettier',
-    'prettier/react',
+    "airbnb-base",
+    "airbnb-base/rules/strict",
+    "airbnb/rules/react",
+    "prettier",
+    "prettier/react"
   ],
-  plugins: ['no-loops'],
+  plugins: ["no-loops", "react-hooks"],
   env: {
     browser: true,
     jest: true,
-    node: true,
+    node: true
   },
   rules: {
-    'global-require': 0,
-    'react/jsx-filename-extension': [2, { 'extensions': ['.js'] }],
-    'react/destructuring-assignment': [2, 'always', { 'ignoreClassFields': true }],
-    'no-loops/no-loops': 2,
-  },
+    "global-require": 0,
+    "react/jsx-filename-extension": [2, { extensions: [".js"] }],
+    "react/destructuring-assignment": [
+      2,
+      "always",
+      { ignoreClassFields: true }
+    ],
+    "no-loops/no-loops": 2,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
 };
