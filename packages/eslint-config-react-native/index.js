039cada88e161fef0993c60be7cbaa4f4111c9e8
Vitaliy Pakshaev <pakshaev.vs@gmail.com>
2019-08-05 13:48:59 +0300

    config files fix

--- packages/eslint-config-react-native/index.js
+++ packages/eslint-config-react-native/index.js
@@ -1,25 +1,25 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["airbnb", "prettier", "prettier/react"],
-  plugins: ["react-native", "no-loops", "react-hooks"],
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react-native', 'no-loops', 'react-hooks'],
   rules: {
-    "global-require": 0,
-    "react/jsx-filename-extension": [2, { extensions: [".js"] }],
-    "react/destructuring-assignment": [
+    'global-require': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.js'] }],
+    'react/destructuring-assignment': [
       2,
-      "always",
-      { ignoreClassFields: true }
+      'always',
+      { ignoreClassFields: true },
     ],
-    "react-native/no-unused-styles": 2,
-    "react-native/split-platform-components": 2,
-    "react-native/no-inline-styles": 2,
-    "react-native/no-color-literals": 2,
-    "react-native/no-raw-text": 2,
-    "no-loops/no-loops": 2,
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'no-loops/no-loops': 2,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
   },
   globals: {
-    __DEV__: true
-  }
-};
+    __DEV__: true,
+  },
+}
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,28 +1,28 @@
 module.exports = {
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   extends: [
-    "airbnb-base",
-    "airbnb-base/rules/strict",
-    "airbnb/rules/react",
-    "prettier",
-    "prettier/react"
+    'airbnb-base',
+    'airbnb-base/rules/strict',
+    'airbnb/rules/react',
+    'prettier',
+    'prettier/react',
   ],
-  plugins: ["no-loops", "react-hooks"],
+  plugins: ['no-loops', 'react-hooks'],
   env: {
     browser: true,
     jest: true,
-    node: true
+    node: true,
   },
   rules: {
-    "global-require": 0,
-    "react/jsx-filename-extension": [2, { extensions: [".js"] }],
-    "react/destructuring-assignment": [
+    'global-require': 0,
+    'react/jsx-filename-extension': [2, { extensions: ['.js'] }],
+    'react/destructuring-assignment': [
       2,
-      "always",
-      { ignoreClassFields: true }
+      'always',
+      { ignoreClassFields: true },
     ],
-    "no-loops/no-loops": 2,
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
-  }
-};
+    'no-loops/no-loops': 2,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+  },
+}
